875cdcb0f39be37d9b0cf29c90ff8bab9bfb599b
Willads Storm-Ritter <git@vardaasen.no>
2024-09-18 13:30:22 +0200

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Willads Storm-Ritter on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
